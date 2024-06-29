0b0c33b5f7ca9cc8bf48d3dac1710fdb15553cb9
Craig CLark <clark.craig@gmail.com>
2024-06-29 16:09:48 -0400

    set up theme and config

---
+++ html/themes/custom/picc/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.picc = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
